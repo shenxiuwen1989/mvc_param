748a1c4d5dc07d22e1f3508e7635792df2b365c0
unknown <shenxiuwen1989@163.com>
2018-10-10 16:38:29 +0800

    f

---
+++ src/main/java/com/sxw/controller/ArrayController.java
@@ -0,0 +1,46 @@
+package com.sxw.controller; /*
+ * @(#)ArrayController.java 1.0 2018/10/10
+ * @Copyright:  Copyright © 2007-2018 ky-express.com.All Rights Reserved.
+ * @Description: 
+ * 
+ * @Modification History:
+ * @Date:        2018/10/10
+ * @Author:      
+ * @Version:     1.0.0.0
+ * @Description: (Initialize)
+ * @Reviewer:    
+ * @Review Date: 
+ */
+
+import com.sxw.entry.Student;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "array")
+public class ArrayController {
+
+    @RequestMapping(value = "post/printPostParam",method = RequestMethod.POST)
+    public String printAarry(@RequestBody Student[] arr){
+        String str = "";
+       for(int i=0,length=arr.length;i<length;i++){
+           str += arr[i].getName()+"&";
+       }
+        return str;
+    }
+
+    /**
+     *
+     *   *  这种只适合post方法
+     *
+     *
+     *  http://localhost:8080/mvc/array/post/printPostParam
+     *  [{"name":"sxw1","age":"121"},{"name":"sxw2","age":"122"}]
+     * 注意：[]表示对象
+     */
+}
