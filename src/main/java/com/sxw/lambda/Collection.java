d3105e951a26e86424437ee2fbecfe7f10af97bf
unknown <shenxiuwen1989@163.com>
2018-10-12 15:07:26 +0800

    s

---
+++ src/main/java/com/sxw/lambda/Collection.java
@@ -0,0 +1,53 @@
+package com.sxw.lambda; /*
+ * @(#)Collection.java 1.0 2018/10/12
+ * @Copyright:  Copyright © 2007-2018 ky-express.com.All Rights Reserved.
+ * @Description: 
+ * 
+ * @Modification History:
+ * @Date:        2018/10/12
+ * @Author:      
+ * @Version:     1.0.0.0
+ * @Description: (Initialize)
+ * @Reviewer:    
+ * @Review Date: 
+ */
+
+import com.sxw.entry.Student;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Collection {
+
+
+    public void testList(List<Student> list){
+
+        //1.拼接
+        String str1 = list.stream().map(m->m.getName()).collect(Collectors.joining("-"));
+        System.out.println(str1);
+
+        StringBuffer str2 = new StringBuffer();
+        list.forEach(m->{
+            str2.append(m.getName()+"-");
+        });
+        System.out.println(str2.toString().substring(0,str2.length()-1));
+
+        //2.获取单个值
+        Optional<Student> swx = list.stream().filter(m -> m.getName().equals("swx")).findFirst();//获取名字为"sxw"的一条数据
+        System.out.println(swx.isPresent());
+
+    }
+
+    public static void main(String[] args) {
+        Collection collection = new Collection();
+
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("sxw",18));
+        list.add(new Student("王华",20));
+        list.add(new Student("李四",31));
+        collection.testList(list);
+    }
+}
