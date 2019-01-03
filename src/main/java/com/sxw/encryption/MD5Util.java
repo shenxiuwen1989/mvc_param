d86b9edc74729910d5fda5ce5d1475d577109e52
申秀文 <shenxiuwen1989@163.com>
2019-01-03 17:33:29 +0800

    去瞧瞧

--- src/main/java/com/sxw/encryption/MD5Util.java
+++ src/main/java/com/sxw/encryption/MD5Util.java
@@ -1,6 +1,7 @@
 package com.sxw.encryption;
 
 import lombok.extern.slf4j.Slf4j;
+import sun.misc.BASE64Encoder;
 
 import java.security.MessageDigest;
 
@@ -12,6 +13,8 @@ public class MD5Util {
 
     /**
      * MD5摘要：接收任意大小的数据，输出固定长度的哈希值
+     * MD5散列值是一般128位的MD5散列被表示为32位十六进制数字
+     另外重申一个概念.MD5不是加密算法.它是hash(散列)算法,它是不可逆的.
      * @param content ：待加密的内容
      * @param encoding ：加密的编码集，如 “UTF-8”,"GBK"
      * @return
@@ -41,8 +44,10 @@ public class MD5Util {
     }
 
 
+
+
     public static void main(String[] args) {
        String  str =  MD5Util.md5Signature("SXW","UTF-8");
-        System.out.println(str);
+        System.out.println(str.toUpperCase());
     }
 }
